4581e828787156271136c3fd61f0e6b51208b3cf
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2021-01-09 17:28:32 +0200

    some tests for lru cache

--- lru-cache/LRU_Cache.py
+++ lru-cache/LRU_Cache.py
@@ -10,6 +10,9 @@ class Node:
 class LRUCache(object):
 
     def __init__(self, capacity):
+        if capacity < 1:
+            raise Exception("Min size is 1")
+
         self.capacity = capacity
         self.cache = {}
         self.head = None
@@ -26,7 +29,12 @@ class LRUCache(object):
 
     def set(self, key, value):
         # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
-        if self.head is None or self.capacity == 1:
+        if key is None:
+            raise Exception("key is invalid")
+        if value is None:
+            raise Exception("value is invalid")
+
+        if self.cache_initialized():
             self.cache[key] = self.head = self.tail = Node(key, value)
         elif key in self.cache.keys():
             node = self.cache[key]
@@ -39,7 +47,7 @@ class LRUCache(object):
                 self.cache[key] = self.head
                 if node is self.tail:
                     self.tail = node.prev
-        elif len(self.cache) == self.capacity:
+        elif self.is_full():
             self.head = Node(key, value, self.head)
             self.cache.pop(self.tail.key)
             self.tail = self.tail.prev
@@ -49,20 +57,53 @@ class LRUCache(object):
             new_node.next.prev = new_node
             self.cache[key] = self.head
 
+    def is_full(self) -> bool:
+        return len(self.cache) == self.capacity
 
-our_cache = LRUCache(5)
+    def cache_initialized(self) -> bool:
+        return self.head is None or self.capacity == 1
 
+
+cache_size = 5
+our_cache = LRUCache(cache_size)
+print("cache size: " + str(cache_size))
+print("set(1,1)")
 our_cache.set(1, 1)
+print("set(2,2)")
 our_cache.set(2, 2)
+print("set(3,3)")
 our_cache.set(3, 3)
+print("set(4,4)")
 our_cache.set(4, 4)
 
+res1 = our_cache.get(1)  # returns 1
+print("get(1) expected val: 1, actual val: " + str(res1))
+res2 = our_cache.get(2)  # returns 2
+print("get(2) expected val: 2, actual val: " + str(res2))
+res3 = our_cache.get(9)  # returns -1 because 9 is not present in the cache
+print("get(9) expected val: -1, actual val: " + str(res3))
 
-our_cache.get(1)       # returns 1
-our_cache.get(2)       # returns 2
-our_cache.get(9)      # returns -1 because 9 is not present in the cache
-
+print("set(5,5)")
 our_cache.set(5, 5)
+print("set(6,6)")
 our_cache.set(6, 6)
 
-our_cache.get(3)    # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+res4 = our_cache.get(3)  # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+print("get(3) expected val: -1, actual val: " + str(res4))
+
+print()
+print("Input validation")
+try:
+    our_cache.set(None, 1)
+except Exception:
+    print("throws exception for invalid key")
+
+try:
+    our_cache.set(1, None)
+except Exception:
+    print("throws exception for invalid value")
+
+try:
+    LRUCache(0)
+except Exception:
+    print("throws exception for invalid LRU cache init size")
