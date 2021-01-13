a8cc3335b6b69e8e2c9229e834998dc6952b3a6d
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2021-01-13 08:57:25 +0200

    tree builder first step

--- huffman-coding/huffman-coding.py
+++ huffman-coding/huffman-coding.py
@@ -3,7 +3,6 @@ from queue import PriorityQueue
 
 
 class Node:
-
     def __init__(self, is_leaf: bool = False, left=None, right=None, count=0, char=None):
         self.is_leaf = is_leaf
         self.left = left
@@ -12,7 +11,15 @@ class Node:
         self.char = char
 
 
-def huffman_encoding(data):
+class Tree:
+    def __init__(self):
+        self.head: Node = None
+
+    def initialized(self) -> bool:
+        return self.head is None
+
+
+def huffman_encoding(data) -> Tree:
     if not data:
         raise Exception("data is not valid")
 
@@ -27,12 +34,26 @@ def huffman_encoding(data):
 
     # priority queue
     priority_q = PriorityQueue()
-
+    # print(priority_q.get())
+    # fill the queue
     for key, value in freq_map.items():
         priority_q.put((value, key))
 
-    while not priority_q.empty():
-        print(priority_q.get())
+    tree = Tree()
+
+    # while not priority_q.empty():
+    #     left = priority_q.get()
+    #     right = priority_q.get()
+    #
+    #     if not tree.initialized() is None:
+    #         left_node = Node(True, count=left[0], char=left[1])
+    #         right_node = Node(True, count=right[0], char=right[1])
+    #         total_count = left_node.count + right_node.count
+    #         head = Node(False, left_node, right_node, total_count)
+    #         tree.head = head
+    #     # elif priority_q.
+
+    return tree
 
 
 def huffman_decoding(data, tree):
